ff6d658bb727868480a12e1a34c4f22a7746d6ec
sheggz <emmanuelikoya99@gmail.com>
2022-08-23 23:25:34 +0000

    implement regular expressions on a log file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\w+)\]\s\[to:(\+?\d*)\]\s\[flags:(-?\d:\d:-?\d:-?\d:-?\d)\]).join(separator=',')
+puts ARGV[0].scan(/\[from:(\w+)\]\s\[to:(\+?\d*)\]\s\[flags:(-?\d:\d:-?\d:-?\d:-?\d)\]/).join(separator=',')
